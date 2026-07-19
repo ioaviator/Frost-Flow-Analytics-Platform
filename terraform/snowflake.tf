75064630a05132035575624eb8fa1949b5bd5dc4
ioaviator <kingsolomonifeanyi@gmail.com>
2026-07-19 10:41:15 +0100

    chore(terraform): create snowflake table

--- terraform/snowflake.tf
+++ terraform/snowflake.tf
@@ -1,15 +1,34 @@
 
-resource "snowflake_database" "fema" {
+resource "snowflake_database" "fema_db" {
   name = "FEMA_DISASTER"
 }
 
-resource "snowflake_schema" "schema" {
+resource "snowflake_schema" "fema_schema" {
   name     = "RAW"
-  database = snowflake_database.fema.name
+  database = snowflake_database.fema_db.name
 }
 
-resource "snowflake_storage_integration_gcs" "fema_storage_int" {
-  name                      = "FEMA_STORAGE_INT"
-  enabled                   = true
-  storage_allowed_locations = ["gcs://frost-flow-data/"]
+resource "snowflake_table" "fema_table" {
+  database                    = snowflake_schema.fema_schema.database
+  schema                      = snowflake_schema.fema_schema.name
+  name                        = "FEMA_INTERVENTION"
+  change_tracking             = false
+ 
+  column {
+    name     = "ingestion_time"
+    type     = "TIMESTAMP_NTZ"
+    nullable = true
+  }
+
+  column {
+    name     = "src_file"
+    type     = "STRING"
+    nullable = true
+  }
+
+  column {
+    name     = "payload"
+    type     = "VARIANT"
+    nullable = false
+  }
 }
