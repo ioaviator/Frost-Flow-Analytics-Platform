161a41df34521aaa9c53db0f4833be3b35bcd109
ioaviator <kingsolomonifeanyi@gmail.com>
2026-07-21 13:10:51 +0100

    fix(api): add timestamp to file format

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 import functions_framework
+import time
 from extract import extract_data
 from load import upload_json_to_gcs
 
@@ -7,6 +8,8 @@ def apiConnect(request):
 
     api_data = extract_data()
 
-    upload_json_to_gcs('fema-disaster-data', 'fema_disaster.json', api_data)
+    timestamp = time.strftime('%Y-%m-%dT%H-%M-%S')
+    file_name = f'fema_disaster_{timestamp}.json'
+    upload_json_to_gcs('fema-disaster-data', file_name, api_data)
 
     return {"status": "success", "message": "Data successfully uploaded to bucket"}, 200
